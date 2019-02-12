481441c9ac7e88d970ec3ff9b8100e85d8f826d2
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-12 20:54:03 +0100

    default props in to do list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from "react";
 
-import ToDoList from "./components/ToDoList";
+import ToDoList from "./containers/ToDoList";
 import "./App.css";
 
 class App extends Component {
-  myTasks = [
-    { done: false, text: "zagrac w wiedzmina" },
-    { done: false, text: "Spacer" },
-    { done: false, text: "trening" },
-    { done: true, text: "zagrac w wiedzmina3" },
-    { done: false, text: "lorem" },
-    { done: false, text: "ipsum" }
-  ];
-
   render() {
     return (
       <div className="App">
-        <ToDoList
-          title="My staff"
-          tasks={this.myTasks}
-          done={this.myTasks.done}
-        />
+        <ToDoList />
       </div>
     );
   }
